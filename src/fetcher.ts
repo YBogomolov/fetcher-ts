f1a27d8d5151426192ed26021c4c8ee7077521f6
Yuriy Bogomolov <yuriy.bogomolov@gmail.com>
2019-12-31 16:46:04 +0300

    Add Functor instance; change fields to methods

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,17 +1,17 @@
 // tslint:disable:no-any
 import { fetch as crossFetch } from 'cross-fetch';
 import * as E from 'fp-ts/lib/Either';
-import { unsafeCoerce } from 'fp-ts/lib/function';
+import { flow, unsafeCoerce } from 'fp-ts/lib/function';
 import { pipe } from 'fp-ts/lib/pipeable';
 import * as io from 'io-ts';
 
-export type Result<Code extends number, A> = { code: Code, data: A };
+export type Result<Code extends number, A> = { code: Code, payload: A };
 
 export type Handled<T, Code extends number> =
   T extends infer R ? R extends Result<any, any> ? R['code'] extends Code ? never : R : never : never;
 
 export type Data<T, Code extends number> =
-  T extends infer R ? R extends Result<any, any> ? R['code'] extends Code ? R['data'] : never : never : never;
+  T extends infer R ? R extends Result<any, any> ? R['code'] extends Code ? R['payload'] : never : never : never;
 
 export type Codes<T> =
   T extends infer R ? R extends Result<any, any> ? R['code'] : never : never;
@@ -21,6 +21,8 @@ export type HandlersMap<TResult, To> = Map<
   [(data: Data<TResult, Codes<TResult>>) => To, io.Type<Data<TResult, Codes<TResult>>> | undefined]
 >;
 
+export type Fetch = typeof fetch;
+
 export class Fetcher<TResult extends Result<any, any>, To> {
   private readonly handlers: HandlersMap<TResult, To> = new Map();
   private restHandler?: () => To = void 0;
@@ -28,29 +30,37 @@ export class Fetcher<TResult extends Result<any, any>, To> {
   constructor(
     private readonly input: RequestInfo,
     private readonly init?: RequestInit,
-    private readonly fetch = crossFetch,
+    private readonly fetch: Fetch = crossFetch,
   ) { }
 
-  handle = <Code extends Codes<TResult>>(
+  map<B>(f: (a: To) => B): Fetcher<TResult, B> {
+    for (const [code, [handler, codec]] of this.handlers) {
+      this.handlers.set(code, unsafeCoerce([flow(handler, f), codec]));
+    }
+
+    return unsafeCoerce(this);
+  }
+
+  handle<Code extends Codes<TResult>>(
     code: Code,
     handler: (data: Data<TResult, Code>) => To,
     codec?: io.Type<Data<TResult, Code>>,
-  ): Fetcher<Handled<TResult, Code>, To> => {
+  ): Fetcher<Handled<TResult, Code>, To> {
     this.handlers.set(code, [handler, codec]);
 
     return unsafeCoerce(this);
   }
 
-  discardRest = (discardWith: () => To): Fetcher<Handled<TResult, never>, To> => {
+  discardRest(discardWith: () => To): Fetcher<Handled<TResult, never>, To> {
     this.restHandler = discardWith;
 
-    return this;
+    return unsafeCoerce(this);
   }
 
-  run = async (): Promise<[To, io.Errors | undefined]> => {
+  async run(): Promise<[To, io.Errors | undefined]> {
     const response = await this.fetch(this.input, this.init);
-    const status = response.status as Codes<TResult>;
 
+    const status = response.status as Codes<TResult>;
     const pair = this.handlers.get(status);
 
     if (pair != null) {
