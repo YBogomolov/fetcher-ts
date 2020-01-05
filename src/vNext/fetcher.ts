e8b6d45e2858ac8a998b8a4c711741ce64a1870f
Yuriy Bogomolov <yuriy.bogomolov@gmail.com>
2020-01-05 18:43:03 +0300

    WIP: vNext

--- src/vNext/fetcher.ts
+++ src/vNext/fetcher.ts
@@ -1,10 +1,13 @@
 import { pipe } from 'fp-ts/lib/pipeable';
-import { ReaderTaskEither } from 'fp-ts/lib/ReaderTaskEither';
+import * as RTE from 'fp-ts/lib/ReaderTaskEither';
+import { map } from 'fp-ts/lib/Record';
 import * as TE from 'fp-ts/lib/TaskEither';
 
 export type Fetch = typeof fetch;
 
-export type Decoder<E, A> = ReaderTaskEither<Response, E, A>;
+export type Decoder<E, A> = RTE.ReaderTaskEither<Response, E, A>;
+
+export type Handlers<S extends Status, E, A> = Record<S, Decoder<E, A>>;
 
 export type Status =
   | 100 | 101 | 102
@@ -14,35 +17,49 @@ export type Status =
   | 400 | 401 | 402 | 403 | 404 | 405 | 406 | 407 | 408 | 409 | 410 | 411 | 412 | 413 | 414 | 415 | 416 | 417 | 418 | 419 | 421 | 422 | 423 | 424 | 426 | 428 | 429 | 431 | 449 | 451 | 499
   | 500 | 501 | 502 | 503 | 504 | 505 | 506 | 507 | 508 | 509 | 510 | 511 | 520 | 521 | 522 | 523 | 524 | 525 | 526;
 
-export type FetcherError =
-  | { readonly type: 'JsonDeserializationError'; readonly details: unknown }
-  | { readonly type: 'HandlerNotSetError'; readonly status: number };
-
 export interface Fetcher<S extends Status, E, A> {
   readonly input: RequestInfo;
-  readonly handlers: Record<S, Decoder<E, A>>;
-  readonly onUnexpectedError: ReaderTaskEither<FetcherError, E, A>;
+  readonly handlers: Handlers<S, E, A>;
+  readonly onUnexpectedError: Decoder<E, A>;
   readonly init?: RequestInit;
 }
 
 export function make<S extends Status, E, A>(
   input: RequestInfo,
-  handlers: Record<S, Decoder<E, A>>,
-  onUnexpectedError: ReaderTaskEither<FetcherError, E, A>,
+  handlers: Handlers<S, E, A>,
+  onUnexpectedError: Decoder<E, A>,
   init?: RequestInit,
 ): Fetcher<S, E, A> {
   return { input, handlers, onUnexpectedError, init };
 }
 
-export function extend<OldS extends Status, NewS extends Status, E, A>(
+export function extendWith<A, B extends A>(ab: (a: A) => B):
+  <OldS extends Status, NewS extends Exclude<Status, OldS>, E>(
+    fetcher: Fetcher<OldS, E, A>,
+    handlers: Handlers<NewS, E, B>,
+    onUnexpectedError?: Decoder<E, B>,
+  ) => Fetcher<OldS | NewS, E, B> {
+  return <OldS extends Status, NewS extends Exclude<Status, OldS>, E>(
+    fetcher: Fetcher<OldS, E, A>,
+    handlers: Handlers<NewS, E, B>,
+    onUnexpectedError?: Decoder<E, B>,
+  ) => ({
+    ...fetcher,
+    handlers: {
+      ...map<Decoder<E, A>, Decoder<E, B>>(RTE.map(ab))(fetcher.handlers) as Record<OldS, Decoder<E, B>>,
+      ...handlers,
+    },
+    onUnexpectedError: onUnexpectedError || RTE.map(ab)(fetcher.onUnexpectedError),
+  });
+}
+
+export function extend<OldS extends Status, NewS extends Exclude<Status, OldS>, E, A>(
   fetcher: Fetcher<OldS, E, A>,
-  handlers: Record<NewS, Decoder<E, A>>,
-  onUnexpectedError?: ReaderTaskEither<FetcherError, E, A>,
+  handlers: Handlers<NewS, E, A>,
 ): Fetcher<OldS | NewS, E, A> {
   return {
     ...fetcher,
     handlers: { ...fetcher.handlers, ...handlers },
-    onUnexpectedError: onUnexpectedError || fetcher.onUnexpectedError,
   };
 }
 
@@ -61,11 +78,9 @@ export function toTaskEither(fetch: Fetch): <S extends Status, E, A>(fetcher: Fe
       TE.chain(
         (response) => {
           const status = response.status;
-          if (isHandled(status)) {
-            return fetcher.handlers[status](response);
-          } else {
-            return fetcher.onUnexpectedError({ type: 'HandlerNotSetError', status });
-          }
+          const method = isHandled(status) ? fetcher.handlers[status] : fetcher.onUnexpectedError;
+
+          return method(response);
         },
       ),
     );
